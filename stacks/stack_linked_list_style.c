b2ffdd27810c144804078d0a0629ff9ad9ba9ac2
Simon Neidhart <simon.neidhart@hotmail.com>
2023-03-26 20:38:11 +0200

    stack linked list style

---
+++ stacks/stack_linked_list_style.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+// implementiere eine Struct für ein Element eines Stacks
+
+// implementiere den Stack selber, welcher auf das jetzige Element zeigt
+
+// implementiere eine Funktion, welche ein Element mit der angegebenen Ganzzahl kreiert
+
+// implementiere eine Funktion, die ein Element auf den Stack legt.
+
+// implementiere eine Funktion "pop" mit Rückgabewert int, welche die Daten des aktuellen Tops des Stacks ausgibt.
+
+// kreiere einen Stack, lege drei Elemente mit den Werten 3, 4 und 5 darauf und pope sie nacheinander vom Stack. Gib die ausgelesenen Werte aus.
+void main()
+{
+    // Stack *stack = malloc(sizeof(Stack));
+    // push(stack, 3);
+    // push(stack, 4);
+    // push(stack, 5);
+    // printf("Current stack element: %d\n", pop(stack));
+    // printf("Current stack element: %d\n", pop(stack));
+    // printf("Current stack element: %d\n", pop(stack));
+}
