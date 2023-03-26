ea9deaa43b47af88a451a3320afa34b7b0a9b0ab
Simon Neidhart <simon.neidhart@hotmail.com>
2023-03-26 20:38:42 +0200

    loesung

--- SW05_list_stack/stack_linked_list_style.c
+++ SW05_list_stack/stack_linked_list_style.c
@@ -1,14 +1,49 @@
 #include <stdlib.h>
 
 // implementiere eine Struct für ein Element eines Stacks
+typedef struct element
+{
+    int data;
+    struct element *previous;
+} Element;
 
 // implementiere den Stack selber, welcher auf das jetzige Element zeigt
+typedef struct stack
+{
+    struct element *top;
+} Stack;
 
 // implementiere eine Funktion, welche ein Element mit der angegebenen Ganzzahl kreiert
+Element *create_element(int data)
+{
+    Element *element = malloc(sizeof(Element));
+    element->previous = NULL;
+    element->data = data;
+}
 
 // implementiere eine Funktion, die ein Element auf den Stack legt.
+void push(Stack *stack, int data)
+{
+    Element *new = create_element(data);
+    if (stack->top == NULL)
+    {
+        stack->top == new;
+        return;
+    }
+
+    new->previous = stack->top;
+    stack->top = new;
+}
 
 // implementiere eine Funktion "pop" mit Rückgabewert int, welche die Daten des aktuellen Tops des Stacks ausgibt.
+int pop(Stack *stack)
+{
+    int data = stack->top->data;
+    Element *freeElement = stack->top;
+    stack->top = freeElement->previous;
+    free(freeElement);
+    return data;
+}
 
 // kreiere einen Stack, lege drei Elemente mit den Werten 3, 4 und 5 darauf und pope sie nacheinander vom Stack. Gib die ausgelesenen Werte aus.
 void main()
