04ece8c3db5ade74f7c481c1f28fe1fcb32d35cf
Simon Neidhart <simon.neidhart@hotmail.com>
2023-03-26 19:01:42 +0200

    SW05: add solution

--- SW05_list_stack/list.c
+++ SW05_list_stack/list.c
@@ -16,10 +16,66 @@ typedef struct list
 } List;
 
 // Schreibe eine Methode append_node, die eine Node am Ende der Liste anfügt
+void append_node(List *list, int data)
+{
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+    if (list->head == NULL)
+    {
+        newNode->next = NULL;
+        list->size = 1;
+        list->head = newNode;
+        list->tail = newNode;
+        return;
+    }
+
+    list->tail->next = newNode;
+    newNode->prev = list->tail;
+    list->tail = newNode;
+    list->size++;
+}
 
 // Schreibe eine Methode print_list_forward, die die Liste vom Anfang (head) zum Ende (tail) ausgibt.
+void print_list_forward(List *list)
+{
+    printf("printing from head to tail\n");
+    if (list->size == 0)
+    {
+        printf("no values in list");
+        return;
+    }
+
+    int index = 0;
+    Node *currentNode = list->head;
+    while (currentNode != NULL)
+    {
+        printf("Node at index %d contains data %d\n", index, currentNode->data);
+        currentNode = currentNode->next;
+        index++;
+    }
+}
 
 // Schreibe eine Methode print_list_backwards, die die Liste vom Ende (tail) zum Anfang (head) ausgibt.
+void print_list_backwards(List *list)
+{
+    printf("printing from tail to head\n");
+    if (list->size == 0)
+    {
+        printf("no values in list");
+        return;
+    }
+
+    int index = list->size - 1;
+    Node *currentNode = list->tail;
+    while (currentNode != NULL)
+    {
+        printf("Node at index %d contains data %d\n", index, currentNode->data);
+        currentNode = currentNode->prev;
+        index--;
+    }
+}
 
 void main()
 {
