1d0185dfb0a0081d7df41d06e06da026107a87b9
Simon Neidhart <simon.neidhart@hotmail.com>
2023-11-28 22:42:14 +0100

    Übung für Pointers

---
+++ pointers/UebungPointers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * Schreibe eine Funktion "add", die zwei Zahlen miteinander addiert und das Resultat zurückgibt.
+*/
+
+/**
+ * Schreibe eine Funktion "apply", die zwei Zahlen und einen Zeiger auf eine Funktion mit zwei Zahlen
+ * entgegennimmt und die Funktion mit den zwei Zahlen aufruft und das Resultat zurückgibt.
+*/
+
+/**
+ * Schreibe eine Funktion "main", die einen Pointer auf die Funktion "add" definiert und danach die Funktion
+ * "apply" mit zwei Zahlen und dem vorher definierten Pointer aufruft und das Resultat in einer Variable
+ * namens "result" speichert.
+ * Rufe danach die folgende Funktion auf, um den Wert auszugeben:
+ * printf("Ergebnis = %d\n", result);
+*/
+int main() {
+
+
+  
+  printf("Ergebnis = %d\n", result);
+
+  return 0;
+}
