a86f8f4da2e53f256a5c28ba380dbe7b53d39592
Simon Neidhart <simon.neidhart@hotmail.com>
2023-11-28 22:43:58 +0100

    Übung für Arrays

---
+++ arrays/exercise1.c
@@ -0,0 +1,14 @@
+void main()
+{
+    char *bookTitles[] = {
+        "Der kleine Prinz",
+        "Harry Potter und der Stein der Weisen",
+        "To Kill a Mockingbird",
+        "1984",
+        "Das Parfum"};
+
+    // gib alle Buchtitel aus (printf("%s\n", <<wert>>))
+
+    // Ändere den Wert "Harry Potter und der Stein der Weisen" im Array auf
+    // "Harry Potter und die Kammer des Schreckens" und gib nochmals alle Werte des Arrays aus.
+}
