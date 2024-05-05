4a9bef4b53e2986062d1ad883219563438c0802f
Simon Neidhart <simon.neidhart@hotmail.com>
2024-05-05 15:34:05 +0200

    TestCases und Method Stub

---
+++ searches/linear_search_array.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+const int NO_KEY = -1;
+
+int linearSearch(int array[], int size, int key)
+{
+    // TODO: Linearen Sortieralgorithmus entwickeln
+
+    return NO_KEY;
+}
+
+void testCases()
+{
+    int array[] = {1, 6, 3, 8, 7, 9, 99, 37, 17};
+    int size = sizeof(array) / sizeof(array[0]);
+    assert(linearSearch(array, size, 3) == 2);
+    assert(linearSearch(array, size, 7) == 4);
+    assert(linearSearch(array, size, 17) == 8);
+    assert(linearSearch(array, size, 18) == NO_KEY);
+
+    int second[] = {};
+    assert(linearSearch(second, 0, 18) == NO_KEY);
+    assert(linearSearch(second, -1, 73) == NO_KEY);
+
+    printf("tests passed, great job!");
+}
+
+int main()
+{
+    testCases();
+
+    return 0;
+}
