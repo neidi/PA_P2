e5684e2065222559a8ceea4009ef3be903ab4fcc
Simon Neidhart <simon.neidhart@hotmail.com>
2024-05-05 16:04:20 +0200

    add exercise and method stub

---
+++ sorts/selection_sort_array.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+void selectionSort(int array[], int size)
+{
+    // TODO: SelectionSort entwickeln
+}
+
+void testCases()
+{
+    int array[] = {3, 6, 1, 5, 5, 7, 2, 9};
+    int size = sizeof(array) / sizeof(array[0]);
+    selectionSort(array, size);
+    assert(array[0] == 1);
+    assert(array[1] == 2);
+    assert(array[2] == 3);
+    assert(array[3] == 5);
+    assert(array[4] == 5);
+    assert(array[5] == 6);
+    assert(array[6] == 7);
+    assert(array[7] == 9);
+
+    printf("tests passed, great job!");
+}
+
+int main()
+{
+    testCases();
+
+    return 0;
+}
