58e0210f1d85a5b6444a89c8bca852721ead48d7
Simon Neidhart <simon.neidhart@hotmail.com>
2025-03-24 14:19:40 +0100

    refactoring: format

--- searches/linear_search_array.c
+++ searches/linear_search_array.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+
 #define RESET   "\033[0m"
 #define GREEN   "\033[32m"      /* Green */
 
-
 const int NO_KEY = -1;
 
-int linearSearch(int array[], int size, int key)
-{
+int linearSearch(int array[], int size, int key) {
     // TODO: Linearen Sortieralgorithmus entwickeln
 
     return NO_KEY;
 }
 
-void testCases()
-{
+void testCases() {
     int array[] = {1, 6, 3, 8, 7, 9, 99, 37, 17};
     int size = sizeof(array) / sizeof(array[0]);
     assert(linearSearch(array, size, 3) == 2);
@@ -27,11 +25,10 @@ void testCases()
     assert(linearSearch(second, 0, 18) == NO_KEY);
     assert(linearSearch(second, -1, 73) == NO_KEY);
 
-    printf( GREEN "tests passed, great job!" RESET);
+    printf(GREEN "tests passed, great job!" RESET);
 }
 
-int main()
-{
+int main() {
     testCases();
 
     return 0;
