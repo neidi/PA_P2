8ba0c64f5de7dde559cdef7a3ee064fd5ffa4677
Simon Neidhart <simon.neidhart@hotmail.com>
2025-03-24 14:25:43 +0100

    add exercise for bubble sort

---
+++ sorts/bubble_sort_array.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#define RESET   "\033[0m"
+#define GREEN   "\033[32m"      /* Green */
+
+void bubbleSort(int array[], int size)
+{
+    // TODO: BubbleSort entwickeln
+}
+
+void testCases()
+{
+    int array[] = {3, 6, 1, 5, 5, 7, 2, 9};
+    int size = sizeof(array) / sizeof(array[0]);
+    bubbleSort(array, size);
+    assert(array[0] == 1);
+    assert(array[1] == 2);
+    assert(array[2] == 3);
+    assert(array[3] == 5);
+    assert(array[4] == 5);
+    assert(array[5] == 6);
+    assert(array[6] == 7);
+    assert(array[7] == 9);
+
+    printf(GREEN "tests passed, great job!" RESET);
+}
+
+int main()
+{
+    testCases();
+
+    return 0;
+}
