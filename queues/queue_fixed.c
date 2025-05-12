bada97043ef138c59316cfe6216f0ea3e17c7f69
Simon Neidhart <simon.neidhart@hotmail.com>
2025-05-12 16:42:03 +0200

    add solution

--- queues/queue_fixed.c
+++ queues/queue_fixed.c
@@ -9,16 +9,26 @@ void init(Queue *q) {
 }
 
 bool isEmpty(Queue *q) {
+    return q->size == 0;
 }
 
 bool isFull(Queue *q) {
+    return q->size == MAX_SIZE;
 }
 
 bool enqueue(Queue *q, int value) {
+    q->rear = (q->rear + 1) % MAX_SIZE;
+    q->data[q->rear] = value;
+    q->size++;
 }
 
 int dequeue(Queue *q) {
+    int value = q->data[q->front];
+    q->front = (q->front + 1) % MAX_SIZE;
+    q->size--;
+    return value;
 }
 
 int peek(Queue *q) {
+    return q->data[q->front];
 }
