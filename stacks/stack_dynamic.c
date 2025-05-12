e8587be6922ae29b3c2f36d92d65f264628d4371
Simon Neidhart <simon.neidhart@hotmail.com>
2025-05-12 15:29:09 +0200

    add solution for dynamic stack

--- stacks/stack_dynamic.c
+++ stacks/stack_dynamic.c
@@ -20,14 +20,39 @@ Element *createElement(int data) {
 }
 
 void push(Stack *stack, int data) {
+    Element *new = createElement(data);
+    if (stack->top == NULL) {
+        stack->top = new;
+        stack->size++;
+        return;
+    }
+
+    new->previous = stack->top;
+    stack->top = new;
+    stack->size++;
 }
 
 // implementiere eine Funktion "pop" mit Rückgabewert int, welche die Daten des aktuellen Tops des Stacks ausgibt.
 int pop(Stack *stack) {
+    if (stack->top == NULL) {
+        return -1;
+    }
+    int data = stack->top->data;
+    Element *freeElement = stack->top;
+    stack->top = freeElement->previous;
+    free(freeElement);
+    stack->size--;
+    return data;
 }
 
 int peek(Stack *stack) {
+    if (stack->top == NULL) {
+        return -1;
+    }
+    int data = stack->top->data;
+    return data;
 }
 
 bool isEmpty(Stack *stack) {
+    return stack->top == NULL;
 }
