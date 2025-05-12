2a240f9e073a41c1c2109a4843db36be388c3fd2
Simon Neidhart <simon.neidhart@hotmail.com>
2025-05-12 16:38:22 +0200

    add solution

--- stacks/stack_fixed.c
+++ stacks/stack_fixed.c
@@ -11,16 +11,34 @@ Stack *createStack() {
 }
 
 int peek(Stack *stack) {
+    if (stack->top == 0) {
+        return -1;
+    }
+    return stack->elements[stack->top - 1];
 }
 
 bool push(Stack *stack, int value) {
+    if (stack->top < MAX_SIZE) {
+        stack->elements[stack->top++] = value;
+        return true;
+    } else {
+        return false;
+    }
 }
 
 int pop(Stack *stack) {
+    if (stack->top > 0) {
+        int topElement = stack->elements[--stack->top];
+        return topElement;
+    } else {
+        return -1;
+    }
 }
 
 bool isFull(Stack *stack) {
+    return stack->top == MAX_SIZE;
 }
 
 bool isEmpty(Stack *stack) {
+    return stack->top == 0;
 }
